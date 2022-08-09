34dc2e3b0b63c94e2223d1cc1fad0b1b255944e5
Jahed Miah <jahed04368@gmail.com>
2022-08-09 11:37:15 +0100

    fix for navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
   const [closeNav, setCloseNav] = useState(false);
@@ -8,7 +9,7 @@ const Navbar = () => {
   return (
     <div className="sticky top-0 z-50">
       <nav className="bg-teal-400 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-        <div className="container  flex flex-wrap justify-between items-center mx-auto m-2 md:m-5">
+        <div className="container flex flex-wrap justify-between items-center mx-auto m-2 md:m-5">
           <Link href="/" className="flex items-center">
             <div>
               <Image
@@ -18,7 +19,7 @@ const Navbar = () => {
                 width={70}
                 height={40}
               />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 Reach Aid <span className="text-red-900">Trust</span>
               </span>
             </div>
@@ -33,79 +34,53 @@ const Navbar = () => {
             aria-expanded="false"
           >
             <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <svg
-              className="hidden w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            <GiHamburgerMenu size={25} />
           </button>
           <div
             className={
               closeNav
-                ? 'hidden w-full md:block md:w-auto'
-                : 'w-full md:block md:w-auto'
+                ? 'w-full md:block md:w-auto'
+                : 'hidden w-full md:block md:w-auto'
             }
             id="mobile-menu"
           >
             <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium text-white">
               <li>
-                <Link
-                  href="/"
-                  className="block py-2 pr-4 pl-3 text-teal-900 font-semibold	  rounded md:bg-transparent md:p-0 dark:text-white"
-                  aria-current="page"
-                >
-                  Home
+                <Link href="/">
+                  <a
+                    className="block py-2 pr-4 pl-3 text-teal-900 font-semibold	  rounded md:bg-transparent md:p-0 dark:text-white"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
                 </Link>
               </li>
               <li>
-                <Link
-                  href="/about"
-                  className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  About
+                <Link href="/about">
+                  <a className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                    About
+                  </a>
                 </Link>
               </li>
               <li>
-                <Link
-                  href="/projects"
-                  className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Projects
+                <Link href="/projects">
+                  <a className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                    Projects
+                  </a>
                 </Link>
               </li>
               <li>
-                <Link
-                  href="/donate"
-                  className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Donate
+                <Link href="/donate">
+                  <a className="block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                    Donate
+                  </a>
                 </Link>
               </li>
               <li>
-                <Link
-                  href="/contact-us"
-                  className="block py-2 pr-4 pl-3  hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Contact
+                <Link href="/contact-us">
+                  <a className="block py-2 pr-4 pl-3  hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                    Contact
+                  </a>
                 </Link>
               </li>
             </ul>
